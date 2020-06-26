fbec72da73a5272b10451af199d976c6210a7f2b
Gerardo <gerardo.cardosa@gmail.com>
2020-06-25 23:21:33 -0700

    feat: Adding Props
    Added properties to the CommentDetails compontent.
    Passed information from the parent to the child component using props
    
    The CommentDetail is using destructuring for the properties

--- components/src/CommentDeail.js
+++ components/src/CommentDeail.js
@@ -1,20 +1,21 @@
 import React from "react";
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = ({author, comment, avatar, timeAgo}) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam yeah!!!
+            {author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">
+            {comment}
+        </div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommponentDetail from './CommentDeail';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommponentDetail />
+            <CommponentDetail author="Sam" comment={faker.lorem.words()} avatar={faker.image.avatar()} timeAgo="Yesterday 2:00AM"/>
+            <CommponentDetail author="Alex" comment={faker.lorem.words()} avatar={faker.image.avatar()} timeAgo="Today 5:00PM"/>
         </div>
     );
 };
