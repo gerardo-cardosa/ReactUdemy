48cbbbe74bcc4994cf62d61db95f3f9a79717a55
Gerardo <gerardo.cardosa@gmail.com>
2020-06-28 00:01:28 -0700

    fix: Moved to a class based component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>hello World!!!</div>
-};
+//Extending from React.Component to integrate React functionality
+class App extends React.Component {
+
+  render(){
+    let pos = ''
+    window.navigator.geolocation.getCurrentPosition(
+      // call backs success
+      (position) => {
+        console.log(position)
+        pos = position.coords.latitude;
+      },
+      //fail call back
+      (err) => console.log(err)
+  
+    );
+    return <div>Latitude: {pos}</div>
+  }
+}
 
 ReactDOM.render(
     <App />,
