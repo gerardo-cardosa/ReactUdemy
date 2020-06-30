0e341e4bd0538741c99e81da692e08041687bc2f
Gerardo <gerardo.cardosa@gmail.com>
2020-06-29 17:35:58 -0700

    feat: Imported Spinner
    Imported the Spinner component and passed the required text

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 //Extending from React.Component to integrate React functionality
 class App extends React.Component {
+  state = {lat: null, long: 40, errorMessage: ''};
 
-  render(){
+  // Lifecycle React component
+  componentWillMount(){
     window.navigator.geolocation.getCurrentPosition(
       // call backs success
-      (position) => console.log(position),
+      (position) => {
+        console.log(position)
+         this.setState({lat: position.coords.latitude})
+        
+      },
       //fail call back
-      (err) => console.log(err)
+      (err) => {
+        console.log(err);
+        this.setState({errorMessage: err.message});
+      }
     );
-    return <div>Latitude: </div>
+  }
+
+  // This is a reequirement from React
+  render(){
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>
+      }
+
+      if(!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat}/>
+      }
+
+      return <Spinner text="Allow location"/>
   }
 }
 
