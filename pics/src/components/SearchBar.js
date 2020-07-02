76702b4cffde94df80db66fa31406978e4536449
Gerardo <gerardo.cardosa@gmail.com>
2020-07-01 22:39:26 -0700

    feat: Added an onChanged event

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React from "react";
 
-class SearchBar extends React.Component{
-    state = {}
+class SearchBar extends React.Component {
+  state = {};
 
-    // Render function
-    render(){
-        return (
-            <div>
-                <form>
-                    <input type="text" />
-                </form>
-            </div>        
-        );
-    }
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  // Render function
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" className="" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
