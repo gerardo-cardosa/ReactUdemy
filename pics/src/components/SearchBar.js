c334790dc61a8face78bf557fe82fee976b07f98
Gerardo <gerardo.cardosa@gmail.com>
2020-07-01 22:28:45 -0700

    feat: Created a new app called Pics
    This react app will take user input and display a list of images

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    state = {}
+
+    // Render function
+    render(){
+        return (
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>        
+        );
+    }
+}
+
+export default SearchBar
