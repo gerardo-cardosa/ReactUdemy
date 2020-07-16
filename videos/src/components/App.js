9889eb91f5f9452faf70e397e7b1f36ea4c5dd44
Gerardo <gerardo.cardosa@gmail.com>
2020-07-15 22:21:31 -0700

    feat: Added a youtube API file
    Added an API for the youtube API calls based on Axios

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
+import youtube from '../apis/youtube'
 
+const KEY = 'AIzaSyCZ15EH4BulbV90quTE_PQMZXNQD9eMMFo';
 
 class App extends React.Component{
     state ={};
 
     onSearchSubmit = async (term)=> {
         console.log(term);
+
+        youtube.get("/search", {
+            params: {
+                q: term,
+                part:"snippet",
+                maxResults: 5, 
+                key: KEY
+            }
+        });
+
     }
 
     render(){
