94cfa70b5e8274732b7f4d08706ace71731fd0a9
Gerardo <gerardo.cardosa@gmail.com>
2020-07-15 21:44:22 -0700

    feat: Added SearchBar, VideoItems, VideoList
    Added the components that will handle most of the functionality

---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VideoItem from './VideItem';
+
+const VideoList = ()=>{
+    return (
+    <div>
+        Video List below:
+        <VideoItem />
+
+    </div>    
+    
+    );
+}
+
+export default VideoList;
