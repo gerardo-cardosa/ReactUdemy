5f9597bbfc82a1a13f88e19bdd14c2352a488753
Gerardo <gerardo.cardosa@gmail.com>
2020-07-23 23:37:13 -0700

    feat: Dropdown
    Completed the drop down functionality
    It will listen to events outside the component to close the
    dropdown.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
-import Search from './components/Search';
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework.'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engs.'
-    },
-    {
-        title: 'How you use React?',
-        content: 'React is used by creating components.'
-    },
-]
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engs.",
+  },
+  {
+    title: "How you use React?",
+    content: "React is used by creating components.",
+  },
+];
+
+const options = [
+  {
+    label: "The color red",
+    value: "red",
+  },
+  {
+    label: "The color green",
+    value: "green",
+  },
+  {
+    label: "The color blue",
+    value: "blue",
+  },
+];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div>
       {/* <Accordion items={items}/> */}
-      <Search />
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      ) : null}
     </div>
   );
 };
