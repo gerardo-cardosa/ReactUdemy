64e095ed19fbdb8f1ce4da8e8a4bd3dc08b1075e
Gerardo <gerardo.cardosa@gmail.com>
2020-08-19 23:43:09 -0700

    refactor: Strings and Auth info
    Created a strings constant file
    Added userID to the auth process

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -43,7 +43,7 @@ class GoogleAuth extends  React.Component {
 
     onAuthChange = (isSignedIn) =>{
         if(isSignedIn){
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         }
         else{
             this.props.signOut();
