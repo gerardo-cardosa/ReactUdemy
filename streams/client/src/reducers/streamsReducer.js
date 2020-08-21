38680fb5cd1656d2915f0b5a0787f3a27a9e6920
Gerardo <gerardo.cardosa@gmail.com>
2020-08-21 00:17:21 -0700

    feat: Updated the reducers
    Using lodash, updated the reducers to handle different events

--- streams/client/src/reducers/streamsReducer.js
+++ streams/client/src/reducers/streamsReducer.js
@@ -5,6 +5,7 @@ import {
   DELETE_STREAM,
   EDIT_STREAM,
 } from "../actions/types";
+import _ from 'lodash';
 
 
 const INITIAL_STATE = {
@@ -13,11 +14,20 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
+        case FETCH_STREAM:
+            return {...state, [action.payload.id]: action.payload}
         case CREATE_STREAM:
             // const newState =  {...state };
             // newState[action.payload.id] = action.payload;
             // return newState;
             return {...state, [action.payload.id]: action.payload}
+        case EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload}
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')};
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
+
         default:
             return state;
     }
