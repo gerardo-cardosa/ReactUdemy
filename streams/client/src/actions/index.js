6ae51730d8dc356286fc49321ab45797d156f507
Gerardo <gerardo.cardosa@gmail.com>
2020-08-23 11:09:23 -0700

    feat: Added stream list functionality
    This will show the list of available streams
    Plus will show buttons on the streas related to the active user.

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -22,10 +22,13 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", {...formValues, userId});
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // programmatic navigation
+  
 };
 
 export const fetchStreams = () => async (dispatch) => {
